be0b6e3770ecc340a88120e2c74807630daf2a84
vanganuruRAMA <115601688+vanganuruRAMA@users.noreply.github.com>
2022-10-12 15:48:48 +0530

    Add files via upload
    adeda file

---
+++ operators_.py
@@ -0,0 +1,141 @@
+# operators
+'''
+arithmetic operators
+comparison operators
+locical operators
+assignnment operators
+bitwise operators
+conditional operators
+'''
+# arithmetic operators
+'''
++
+-
+*
+/
+%
+**
+//
+'''
+a=10
+b=5
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+print(a%b)
+print(a**b)
+print(a//b)
+# comparison operators
+'''
+==
+!=
+>
+<
+>=
+<=
+'''
+s=5
+y=10
+print(s==y)
+print(s!=y)
+print(s>y)
+print(s<y)
+print(s>=y)
+print(s<=y)
+# logical operators
+'''
+and 
+or
+not
+'''
+x=12
+z=15
+'''
+print(x and z)
+print(x or z)
+print(x not z)
+'''
+print("59__>",x>10 and x<15)
+print("60__>",x>10 or x<5)
+print("61__>",not(x>10 and x<5))
+# assignnment operators
+'''
++=
+-=
+*=
+/=
+%=
+**=
+//=
+'''
+k=10
+n=45
+k+=n
+print(k)
+k-=n
+print(k)
+k*=n
+print(k)
+k/=n
+print(k)
+k**=n
+print(k)
+k//=n
+print(k)
+c=k + n
+print(c)
+c=k - n
+print(c)
+c=k * n
+print(c)
+c=k / n
+print(c)
+c=k % n
+print(c)
+#c=k ** n
+#print(c)
+c=k // n
+print(c)
+# bitwise operators
+'''
+&
+|
+^
+~
+>>
+<<
+'''
+# conditional operators
+'''
+'''
+
+#comparison
+a=12
+b=34
+print(a!=b)
+print(a==b)
+print(a<=b)
+print(a<<b)
+print(a>b)
+print(a>=b)
+#logical
+#and,or,not
+x=10
+y=12
+#print(x.and.12)
+print("59__>",x>10 and y>12)
+print("60__>",x<52 or y<12)
+print("61__>",not (x<10 and y<12))
+
+#assingen
+k=23
+l=12
+k+=l
+print(k)
+k-=l
+print(k)
+k*=l
+print(k)
+
+  
